bd50fe85f5a2321d663811b0e26b335afbfd6a92
tomonori takada <tomo@tomonorinoMacBook-Pro.local>
2019-05-04 16:05:53 +0900

    経過時間カウントクラス実装,collider不要のvirtualPad実装

--- Assets/scripts/MyUnityFrameworks/myFramework/controller/MyVirtualPad.cs
+++ Assets/scripts/MyUnityFrameworks/myFramework/controller/MyVirtualPad.cs
@@ -2,60 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyVirtualPad : MyBehaviour {
-    public ControllerType mControllerType = ControllerType.shortTail;
-    private Vector2 mDragOriginPoint;
-    private bool mDragFlag = false;
-    //ドラッグした方向
-    private Vector2 mTailVec;
-    //タップしたフラグ
-    private bool mTapFlag = false;
-    //<summary>テールの最大長(shortTailモードの時のみ有効)</summary>
-    public float mMaxTailLength = 100;
-    //<summary>ドラッグした判定とする最低距離</summary>
-    public float mThresholdDrag = 10;
-    //<summary>ドラッグした方向</summary>
-    public Vector2 mVector{
-        get { return mTailVec; }
-    }
-    //<summary>ドラッグした方向の単位ベクトル</summary>
-    public Vector2 mNormalized{
-        get { return mTailVec.normalized; }
-    }
-    //<summary>タップした</summary>
-    public bool mIsTapped{
-        get { return mTapFlag; }
-    }
+public class MyVirtualPad : MyPad {
     protected void OnMouseDrag(){
-        Vector2 tMousePosition = Input.mousePosition;
-        Vector2 tTailVec = tMousePosition - mDragOriginPoint;
-        //ドラッグ距離が短いならドラッグしていない扱い
-        if (tTailVec.magnitude < mThresholdDrag) return;
-        //ドラッグした
-        mDragFlag = true;
-        mTailVec = tTailVec;
-        //テールの長さ調整
-        if(mControllerType==ControllerType.shortTail){
-            if(mMaxTailLength<mTailVec.magnitude){
-                //ドラッグの基点修正
-                mDragOriginPoint = tMousePosition + (mDragOriginPoint - tMousePosition).normalized * mMaxTailLength;
-                mTailVec = tMousePosition - mDragOriginPoint;
-            }
-        }
+        mouseDrag();
     }
     protected void OnMouseDown(){
-        mDragOriginPoint = Input.mousePosition;
+        mouseDown();
     }
     protected void OnMouseUp(){
-        mTailVec = Vector2.zero;
-        if (!mDragFlag)
-            mTapFlag = true;
-        mDragFlag = false;
-    }
-    private void FixedUpdate(){
-        mTapFlag = false;
-    }
-    public enum ControllerType{
-        shortTail,longTail
+        mouseUp();
     }
 }
